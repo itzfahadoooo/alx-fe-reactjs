71c6b79124aee5b7af13ecb8ca89d596163f0e58
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-03 22:41:56 +0100

    Initial commit

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,15 +1,21 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import WelcomeMessage from './components/WelcomeMessage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
