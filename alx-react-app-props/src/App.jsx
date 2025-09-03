74e124429cfde0ad77ac25aaeacec9ac63f5e75e
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-03 19:45:40 +0100

    Initial commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,14 +5,16 @@ import UserProfile from './components/UserProfile';
 import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <>
+    <UserContext.Provider value={userData}>
       <Header />
       <WelcomeMessage />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
-    </>
+    </UserContext.Provider>
   );
 }
 
