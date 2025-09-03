82163c0628cd54a08cb9ae8fe64a6a2ea4412736
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-03 19:20:54 +0100

    Initial commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f0f0f0', padding: '20px' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
