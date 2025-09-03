ffd19e5dc9ba62092edd5dec0172406f21cf5cf7
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-03 20:29:24 +0100

    Initial commit

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
-const UserProfile = (props) => {
+import { useContext } from 'react';
+import UserContext from '../UserContext';
+
+const UserProfile = () => {
+  const userData = useContext(UserContext);
+
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px' }}>
+      <h2 style={{ color: 'blue', marginBottom: '5px' }}>{userData.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{userData.age ?? "N/A"}</span></p>
+      <p style={{ fontStyle: 'italic' }}>Bio: {userData.bio ?? "No bio provided"}</p>
     </div>
   );
 };
