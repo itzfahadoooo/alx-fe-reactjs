e129896386035ac83ccce4edce1dd5d1e9a984f8
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-03 19:40:45 +0100

    Initial commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import WelcomeMessage from './components/WelcomeMessage';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <WelcomeMessage />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter/>
       <Footer />
     </>
   );
