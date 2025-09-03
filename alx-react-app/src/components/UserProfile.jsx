6f0fe3bce3db39ad2c2a5243bcfe27fde03b0341
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-03 19:14:40 +0100

    Initial commit

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,10 @@
-const UserProfile = (props) => {
+function UserProfile(props) {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px' }}>
+      <h2 style={{ color: 'blue', marginBottom: '5px' }}>{props.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+      <p style={{ fontStyle: 'italic' }}>Bio: {props.bio}</p>
     </div>
   );
-};
-
+}
 export default UserProfile;
