4f42bf393f493f934bc574a3b73cf95a0e36fc89
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-12 19:36:14 +0100

    Initial commit

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,17 +2,20 @@ import { Link } from "react-router-dom";
 import { useRecipeStore } from "../store/recipeStore";
 
 export default function RecipeList() {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const recipes = useRecipeStore((s) =>
+    s.searchTerm ? s.filteredRecipes : s.recipes
+  );
 
   return (
     <div>
       <h2>Recipes</h2>
-      {recipes.length === 0 && <p>No recipes yet!</p>}
+      {recipes.length === 0 && <p>No recipes found!</p>}
       {recipes.map((recipe) => (
         <div key={recipe.id}>
           <Link to={`/recipe/${recipe.id}`}>
             <h3>{recipe.title}</h3>
           </Link>
+          <p>{recipe.description}</p>
         </div>
       ))}
     </div>
