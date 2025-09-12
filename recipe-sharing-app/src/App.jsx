6859aa20675caf7262356f72adb18a46ea37c493
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-12 19:20:10 +0100

    Initial commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,12 @@
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
