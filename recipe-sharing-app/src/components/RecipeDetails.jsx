d4d06961f70a50734bba8b46514f8b9aef3b07f0
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-12 19:37:50 +0100

    Initial commit

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -8,6 +8,7 @@ export default function RecipeDetails() {
   const recipe = useRecipeStore((s) =>
     s.recipes.find((r) => r.id === Number(id))
   );
+  const addFavorite = useRecipeStore((s) => s.addFavorite);
 
   if (!recipe) return <p>Recipe not found!</p>;
 
@@ -15,6 +16,7 @@ export default function RecipeDetails() {
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
+      <button onClick={() => addFavorite(recipe.id)}>Add to Favorites</button>
       <EditRecipeForm recipe={recipe} />
       <DeleteRecipeButton id={recipe.id} />
       <Link to="/">Back</Link>
