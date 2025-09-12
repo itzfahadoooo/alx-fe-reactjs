0b390db86e71abf2681a667acf6d94c76925af83
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-12 19:38:00 +0100

    Initial commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,8 @@ import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import SearchBar from "./components/SearchBar";
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
 
 export default function App() {
   return (
@@ -16,6 +18,8 @@ export default function App() {
               <AddRecipeForm />
               <SearchBar />
               <RecipeList />
+              <FavoritesList />
+              <RecommendationsList />
             </>
           }
         />
