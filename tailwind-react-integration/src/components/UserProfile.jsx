e1fb57afe063dfb6a609e022a7648ab38ad7a5db
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-09-27 21:56:35 +0100

    Initial commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
+        className="w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300">
         John Doe
@@ -15,4 +15,5 @@ function UserProfile() {
     </div>
   );
 }
+
 export default UserProfile;
