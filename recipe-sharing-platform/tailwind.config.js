9e44ffc92fa7d2c686c9580ce313c79bcce74574
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-10-03 00:06:34 +0100

    Initial commit

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",               // Root HTML file
+    "./src/**/*.{js,jsx,ts,tsx}", // All React components
   ],
   theme: {
     extend: {},
