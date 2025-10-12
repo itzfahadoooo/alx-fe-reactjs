14d01e65139777d6d06537ad45ad6df774734fa8
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-10-12 10:56:47 +0100

    Initial commit

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,11 @@
+import RegistrationForm from "./components/RegistrationForm";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
