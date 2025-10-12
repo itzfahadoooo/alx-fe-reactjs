6d7d613d9b37673e72ac12d4b50bb20cd4cc13d6
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-10-12 11:00:03 +0100

    Initial commit

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,9 +1,10 @@
-import RegistrationForm from "./components/RegistrationForm";
+// import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <RegistrationForm />
+      <FormikForm />
     </div>
   );
 }
