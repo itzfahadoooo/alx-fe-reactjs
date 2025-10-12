d9b91e2732a63fb2c4065bb4ea8a3a5224a91104
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-10-12 19:34:54 +0100

    Initial commit

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,8 +1,8 @@
 /* eslint-env jest */
+import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import TodoList from "../components/TodoList";
-
 
 test("renders initial todos", () => {
   render(<TodoList />);
@@ -13,23 +13,38 @@ test("renders initial todos", () => {
 test("adds a new todo", () => {
   render(<TodoList />);
   const input = screen.getByTestId("todo-input");
-  const button = screen.getByTestId("add-btn");
+  const addBtn = screen.getByTestId("add-btn");
+
   fireEvent.change(input, { target: { value: "Test new todo" } });
-  fireEvent.click(button);
+  fireEvent.click(addBtn);
+
+  // new todo should be in document
   expect(screen.getByText("Test new todo")).toBeInTheDocument();
 });
 
 test("toggles todo completion", () => {
   render(<TodoList />);
-  const todo = screen.getByText("Learn React");
-  fireEvent.click(todo);
-  expect(todo).toHaveStyle("text-decoration: line-through");
+  const todoItem = screen.getByText("Learn React");
+
+  // toggle on (line-through)
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+
+  // toggle off
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: none");
 });
 
 test("deletes a todo", () => {
   render(<TodoList />);
-  const todo = screen.getByText("Learn React");
-  const deleteButton = screen.getByTestId(/delete-/i);
-  fireEvent.click(deleteButton);
-  expect(todo).not.toBeInTheDocument();
+  // find the todo and its delete button
+  const todoText = "Learn React";
+  const todo = screen.getByText(todoText);
+  const deleteBtn = screen.getByTestId("delete-btn-1");
+
+  // click delete
+  fireEvent.click(deleteBtn);
+
+  // the todo should no longer be present
+  expect(screen.queryByText(todoText)).not.toBeInTheDocument();
 });
